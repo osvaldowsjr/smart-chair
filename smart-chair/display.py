057d1ba68ea537c87c3219dba54a9445f610c00b
osvaldowsjr <osvaldowilliam@outlook.com>
2020-09-02 19:35:05 -0300

    tkInter

--- smart-chair/display.py
+++ smart-chair/display.py
@@ -7,24 +7,26 @@ from stopwatch import Stopwatch
 from viewModel.dataManager import dataManager
 from viewModel.timeManager import timeManager
 from viewModel.temperatureManager import tempManager
+from view.interface import Application
+import tkinter
 import gpiozero
-
 
 # receiver = Receiver(port="COM4", baudrate=115200,
 #                   bytesize=8, timeout=2)
 
 def dealAlarm(data_manager: dataManager, timer_idle: Stopwatch, timer_stand):
-    alarm_manager = timeManager(data_manager, timer_idle,timer_stand)
+    alarm_manager = timeManager(data_manager, timer_idle, timer_stand)
     alarm_manager.tryStartTimer()
     alarm_manager.tryStopTimer()
     if alarm_manager.shouldAlarm():
-        led.on()
+        # led.on()
+        print("led on")
         delay = input("Adiar? (s/n)? ")
         if delay.upper() == 'S':
             alarm_manager.delayTimer()
-            led.off()
+            print("led off")
         else:
-            led.on()
+            print("led on")
 
 
 def dealTemp(temp: float):
@@ -32,30 +34,33 @@ def dealTemp(temp: float):
     print(temp_manager.getDisplayMsg())
 
 
+def bt0():
+    randH = uniform(0.0, 10.0)
+    randT = uniform(9.0, 40.0)
+    randL = uniform(0.0, 5.0)
+    new_json = '{{"humidity":{0},"temperature": {1}, "luminosity": {2}, "presence": {3}, "time": {4}}}'.format(
+        randH,
+        randT,
+        randL,
+        "false",
+        1200)
+    print(new_json)
+    return dataManager(new_json)
+
+
 if __name__ == '__main__':
     sw = Stopwatch()
     sw.stop()
     sw2 = Stopwatch()
     sw2.stop()
-    led = gpiozero.LED(17)
+    root = tkinter.Tk()
+    view = Application(root)
+    view.mainloop()
+    # led = gpiozero.LED(17)
     while 1:
         # info = receiver.receiveInfo()
         # dm = dataManager(info)
-        presence_val = input("Press 0 or 1")
-        if presence_val is "0":
-            led.off()
-
-        randH = uniform(0.0, 10.0)
-        randT = uniform(9.0, 40.0)
-        randL = uniform(0.0, 5.0)
-        presence = ["false", "true"]
-        new_json = '{{"humidity":{0},"temperature": {1}, "luminosity": {2}, "presence": {3}, "time": {4}}}'.format(
-            randH,
-            randT,
-            randL,
-            presence[int(presence_val)],
-            1200)
-        print(new_json)
-        dm = dataManager(new_json)
-        dealAlarm(dm, sw,sw2)
+        dm = bt0()
+        view.button0(dm=dm)
+        dealAlarm(dm, sw, sw2)
         dealTemp(dm.getTemperature())
--- smart-chair/view/interface.py
+++ smart-chair/view/interface.py
@@ -1,19 +1,39 @@
 import tkinter as tk
+from random import uniform
 
 from viewModel.dataManager import dataManager
 
 
-class Application(tk.Frame, dataManager):
+class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.titleTemp = tk.Label()
-        self.titleHum = tk.Label()
-        self.temp = tk.Label()
-        self.hum = tk.Label()
-        self.dataManager = dataManager
         self.master = master
-        self.pack()
-        self.create_widgets()
+        self.dm = None
+        self.tempVar = tk.StringVar()
+        self.humVar = tk.StringVar()
+        self.titleTemp = tk.Label(master, text='Temperatura:', bg="red", fg="white")
+        self.temp = tk.Label(master, textvariable=self.tempVar, bg="red", fg="white")
 
-    def create_widgets(self):
-        self.titleTemp.text = 'Temperatura:'
+        self.titleHum = tk.Label(master, text='Umidade:', bg="red", fg="white")
+        self.hum = tk.Label(master, textvariable=self.humVar, bg="green", fg="white")
+
+        self.randButton0 = tk.Button(master, text="0", bg="green")
+        self.randButton1 = tk.Button(master, text="1", bg="red")
+
+        self.dataManager = dataManager
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.titleHum.pack(fill=tk.X)
+        self.hum.pack(fill=tk.X)
+        self.titleTemp.pack(fill=tk.X)
+        self.temp.pack(fill=tk.X)
+        self.randButton0.pack(fill=tk.X)
+        self.randButton1.pack(fill=tk.X)
+
+    def update_widgets(self):
+        self.tempVar.set(self.dm.getTemperature())
+        self.humVar.set(self.dm.getHumidity())
+
+    def button0(self, dm: dataManager):
+        self.dm = dm
